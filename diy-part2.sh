7c98910f0a9214fe6e3adc4f59528294c1f40322
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-03-22 16:43:23 +0800

    Create diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
