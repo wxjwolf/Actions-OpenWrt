124829fcf159fcb85309aeae6c93995343e5d7bb
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-03-23 12:48:16 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -7,8 +7,8 @@ git clone https://github.com/sbwml/luci-app-alist package/alist
 #替换luci-theme-argon和luci-app-argon-config
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-rm -rf package/luci/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 #修改默认登录地址：192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
