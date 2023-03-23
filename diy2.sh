4635d9af332439bfb8fcf3ee3962911b785bccd6
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-03-23 11:43:22 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -3,3 +3,11 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 19.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-alist package/alist
+
+#替换luci-theme-argon和luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+rm -rf package/luci/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci/luci-app-argon-config
+
+
