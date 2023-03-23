6d4ad3f803f836cc187c32b28bffa6b39ea8475c
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-03-23 12:05:15 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -10,4 +10,13 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/lu
 rm -rf package/luci/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci/luci-app-argon-config
 
+#修改默认登录地址：192.168.2.1
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#修改默认主题为luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+#修改默认root登录samba
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+
 
