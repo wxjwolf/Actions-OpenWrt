75e82e052ac71c84ddda06d7a5b632294e13f4be
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-04-04 19:45:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,6 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-#sed -i '$a src-git mydiy https://github.com/wxjwolf/myopenwrt.git' feeds.conf.default
+sed -i '$a src-git mydiy https://github.com/wxjwolf/myopenwrt.git' feeds.conf.default
 
 
