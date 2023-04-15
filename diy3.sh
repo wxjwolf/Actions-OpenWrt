b7d8cf87cfc5f6fd96f923bc6870c6ac99acc98d
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-04-15 15:13:58 +0800

    Update diy3.sh

--- diy3.sh
+++ diy3.sh
@@ -1,8 +1,8 @@
 # 为alist插件更换最新的golang版本
 #update golang 19.x (Fix build for openwrt-21.02/22.03 branches)
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 19.x feeds/packages/lang/golang
-#git clone https://github.com/sbwml/luci-app-alist package/alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 19.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/luci-app-alist package/alist
 
 #替换luci-theme-argon和luci-app-argon-config
 #rm -rf feeds/luci/themes/luci-theme-argon
