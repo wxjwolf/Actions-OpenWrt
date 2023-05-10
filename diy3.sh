0f3aae858bfed7fd87ae0e2d5d9f32f5f9ea578a
wxjwolf <70838099+wxjwolf@users.noreply.github.com>
2023-05-10 19:42:38 +0800

    Update diy3.sh

--- diy3.sh
+++ diy3.sh
@@ -11,7 +11,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 # 更改内核版本
-sed -i 's#^.*KERNEL_PATCHVER:=.*$#KERNEL_PATCHVER:=6.1#' target/linux/x86/Makefile
+#sed -i 's#^.*KERNEL_PATCHVER:=.*$#KERNEL_PATCHVER:=5.15#' target/linux/x86/Makefile
 
 #修改默认登录地址：192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -29,7 +29,7 @@ sed -i $'s/${vendorid:+-V \"$vendorid\"}/-V \'\'/' package/network/config/netifd
 sed -i "s/OpenWrt /Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 #增加lan桥接端口
-sed -i "s/exit 0/uci set network.lan.ifname=\'eth0 eth2\'\nuci commit network\nexit 0/" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/exit 0/uci set network.lan.ifname=\'eth0 eth2\'\nuci commit network\nexit 0/" package/lean/default-settings/files/zzz-default-settings
 
 #修改主页显示时间格式
 sed -i 's/os.date()/os.date(\"%Y年%m月%d日 %X\")/' package/feeds/luci/luci-mod-admin-full/luasrc/view/admin_status/index.htm
